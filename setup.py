515db21534ea2c4cf302a799af4fc0d9890ed897
ZepmanBC <zepman@gmail.com>
2019-06-22 14:59:17 +0200

    Bump version: 0.1.5 → 1.0.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zepmanbc/cookiecutterpackage',
-    version='0.1.5',
+    version='1.0.0',
     zip_safe=False,
 )
